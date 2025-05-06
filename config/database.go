96a382782f3a822cc2c84e7904e5ad6af8b3f69b
R4P8 <rizqirafa45@gmail.com>
2025-05-06 14:45:57 +0700

    refactor: use environment variables for database configuration

--- config/database.go
+++ config/database.go
@@ -4,22 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "admin123"
-	dbname   = "product_db"
 )
 
 var DB *sql.DB
 
 func DatabaseConnection() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
