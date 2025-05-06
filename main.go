2a7ba421b0bc1f1008fea54676a971248fbeea68
R4P8 <rizqirafa45@gmail.com>
2025-05-06 18:50:47 +0700

    feat: load environment variables from .env using godotenv

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"curd-web-go/routes"
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, relying on container env variables")
+	}
+
 	config.DatabaseConnection()
 	if config.DB == nil {
 		log.Fatal("Database connection failed!")
