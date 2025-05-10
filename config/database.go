465a9c5e9c7b6294de348070592456c5876eb951
R4P8 <rizqirafa45@gmail.com>
2025-05-10 22:47:09 +0700

    feat: instrument database connection with otelsql

--- config/database.go
+++ config/database.go
@@ -6,35 +6,36 @@ import (
 	"log"
 	"os"
 
+	"github.com/XSAM/otelsql"
 	_ "github.com/lib/pq"
+	"go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 var DB *sql.DB
 
 func DatabaseConnection() {
-
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	// Inisialisasi variabel global DB
-	var err error
-	DB, err = sql.Open("postgres", psqlInfo)
+	driverName, err := otelsql.Register("postgres", otelsql.WithAttributes(semconv.DBSystemPostgreSQL))
 	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
+		log.Fatalf("Register otelsql driver failed: %v", err)
 	}
 
-	// Cek koneksi database
-	err = DB.Ping()
+	DB, err = sql.Open(driverName, dsn)
 	if err != nil {
-		log.Fatalf("Database connection failed: %v", err)
+		log.Fatalf("DB connection error: %v", err)
 	}
 
-	fmt.Println("Successfully connected to database!")
+	if err = DB.Ping(); err != nil {
+		log.Fatalf("Ping failed: %v", err)
+	}
+
+	log.Println("Connected to DB")
 }
