1d4a1e8cca0df80793f447ab463d6e7da1677cc5
R4P8 <rizqirafa45@gmail.com>
2025-05-19 10:15:11 +0000

    feat(routes): add tracing middleware wrapper to all routes

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"curd-web-go/controllers/categorycontrollers"
 	"curd-web-go/controllers/homecontrollers"
 	"curd-web-go/controllers/productcontrollers"
+	"curd-web-go/middleware"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -11,21 +12,23 @@ import (
 func Routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.GET("/", homecontrollers.Homecontrollers)
+	router.Handler("GET", "/", middleware.Wrap(homecontrollers.Homecontrollers, "Home"))
 
-	router.GET("/categories", categorycontrollers.Index)
-	router.GET("/categories/add", categorycontrollers.Add)
-	router.POST("/categories/add", categorycontrollers.Add)
-	router.GET("/categories/edit", categorycontrollers.Edit)
-	router.POST("/categories/edit", categorycontrollers.Edit)
-	router.GET("/categories/delete", categorycontrollers.Delete)
+	router.Handler("GET", "/categories", middleware.Wrap(categorycontrollers.Index, "CategoriesIndex"))
+	router.Handler("GET", "/categories/add", middleware.Wrap(categorycontrollers.Add, "CategoriesAddGet"))
+	router.Handler("POST", "/categories/add", middleware.Wrap(categorycontrollers.Add, "CategoriesAddPost"))
+	router.Handler("GET", "/categories/edit", middleware.Wrap(categorycontrollers.Edit, "CategoriesEditGet"))
+	router.Handler("POST", "/categories/edit", middleware.Wrap(categorycontrollers.Edit, "CategoriesEditPost"))
+	router.Handler("GET", "/categories/delete", middleware.Wrap(categorycontrollers.Delete, "CategoriesDelete"))
 
-	router.GET("/product", productcontrollers.Index)
-	router.GET("/product/add", productcontrollers.Add)
-	router.POST("/product/add", productcontrollers.Add)
-	router.GET("/product/edit", productcontrollers.Edit)
-	router.POST("/product/edit", productcontrollers.Edit)
-	router.GET("/product/delete", productcontrollers.Delete)
+	router.Handler("GET", "/product", middleware.Wrap(productcontrollers.Index, "ProductIndex"))
+	router.Handler("GET", "/product/add", middleware.Wrap(productcontrollers.Add, "ProductAddGet"))
+	router.Handler("POST", "/product/add", middleware.Wrap(productcontrollers.Add, "ProductAddPost"))
+	router.Handler("GET", "/product/edit", middleware.Wrap(productcontrollers.Edit, "ProductEditGet"))
+	router.Handler("POST", "/product/edit", middleware.Wrap(productcontrollers.Edit, "ProductEditPost"))
+	router.Handler("GET", "/product/delete", middleware.Wrap(productcontrollers.Delete, "ProductDelete"))
 
 	return router
 }
+
+
