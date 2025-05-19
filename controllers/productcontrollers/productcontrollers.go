7d5cbb96a4eae33874d9742cd1025f129739c08b
R4P8 <rizqirafa45@gmail.com>
2025-05-19 13:14:51 +0000

    feat: add context support to product controllers functions

--- controllers/productcontrollers/productcontrollers.go
+++ controllers/productcontrollers/productcontrollers.go
@@ -1,170 +1,178 @@
 package productcontrollers
 
 import (
-	"curd-web-go/config"
-	"curd-web-go/entities"
-	"curd-web-go/models/categorymodels"
-	"curd-web-go/models/productmodels"
-	"html/template"
-	"net/http"
-	"strconv"
-	"time"
+        "curd-web-go/config"
+        "curd-web-go/entities"
+        "curd-web-go/models/categorymodels"
+        "curd-web-go/models/productmodels"
+        "html/template"
+        "net/http"
+        "strconv"
+        "time"
 
-	"github.com/julienschmidt/httprouter"
+        "github.com/julienschmidt/httprouter"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	Products := productmodels.GetAll()
-	data := map[string]any{
-		"Products": Products,
-	}
+        ctx := r.Context()
+        Products := productmodels.GetAll(ctx)
 
-	temp, err := template.ParseFiles("views/products/index.html")
-	if err != nil {
-		panic(err)
-	}
+        data := map[string]any{
+                "Products": Products,
+        }
 
-	err = temp.Execute(w, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+        temp, err := template.ParseFiles("views/products/index.html")
+        if err != nil {
+                panic(err)
+        }
+
+        err = temp.Execute(w, data)
+        if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
 }
 
 func Add(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	if r.Method == "GET" {
-		categories := categorymodels.GetAll()
-		data := map[string]any{
-			"categories": categories,
-		}
-		temp, err := template.ParseFiles("views/products/create.html")
-		if err != nil {
-			panic(err)
-		}
-		temp.Execute(w, data)
-		return
-	}
+        ctx := r.Context()
 
-	if r.Method == "POST" {
-		var product entities.Product
+        if r.Method == "GET" {
+                categories := categorymodels.GetAll(ctx)
+                data := map[string]any{
+                        "categories": categories,
+                }
+                temp, err := template.ParseFiles("views/products/create.html")
+                if err != nil {
+                        panic(err)
+                }
+                temp.Execute(w, data)
+                return
+        }
 
-		// Ambil data dari form
-		product.Name = r.FormValue("name")
-		product.Description = r.FormValue("description")
+        if r.Method == "POST" {
+                var product entities.Product
 
-		// Convert stock dan price ke int & float
-		stock, err := strconv.Atoi(r.FormValue("stock"))
-		if err != nil {
-			http.Error(w, "Invalid stock value", http.StatusBadRequest)
-			return
-		}
-		product.Stock = stock
+                // Ambil data dari form
+                product.Name = r.FormValue("name")
+                product.Description = r.FormValue("description")
 
-		price, err := strconv.ParseFloat(r.FormValue("price"), 64)
-		if err != nil {
-			http.Error(w, "Invalid price value", http.StatusBadRequest)
-			return
-		}
-		product.Price = price
+                // Convert stock dan price ke int & float
+                stock, err := strconv.Atoi(r.FormValue("stock"))
+                if err != nil {
+                        http.Error(w, "Invalid stock value", http.StatusBadRequest)
+                        return
+                }
+                product.Stock = stock
 
-		categoryId, err := strconv.Atoi(r.FormValue("category_id"))
-		if err != nil {
-			http.Error(w, "Invalid category ID", http.StatusBadRequest)
-			return
-		}
+                price, err := strconv.ParseFloat(r.FormValue("price"), 64)
+                if err != nil {
+                        http.Error(w, "Invalid price value", http.StatusBadRequest)
+                        return
+                }
+                product.Price = price
 
-		product.CategoryID = uint(categoryId)
+                categoryId, err := strconv.Atoi(r.FormValue("category_id"))
+                if err != nil {
+                        http.Error(w, "Invalid category ID", http.StatusBadRequest)
+                        return
+                }
 
-		product.CreatedAt = time.Now()
-		product.UpdatedAt = time.Now()
+                product.CategoryID = uint(categoryId)
 
-		if ok := productmodels.Create(product); !ok {
-			// gagal insert
-			temp, _ := template.ParseFiles("views/products/create.html")
-			temp.Execute(w, map[string]any{"error": "Gagal menyimpan produk"})
-			return
-		}
+                product.CreatedAt = time.Now()
+                product.UpdatedAt = time.Now()
 
-		http.Redirect(w, r, "/product", http.StatusSeeOther)
-	}
+                if ok := productmodels.Create(ctx, product); !ok {
+                        // gagal insert
+                        temp, _ := template.ParseFiles("views/products/create.html")
+                        temp.Execute(w, map[string]any{"error": "Gagal menyimpan produk"})
+                        return
+                }
+
+                http.Redirect(w, r, "/product", http.StatusSeeOther)
+        }
 }
 
 func Edit(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	if r.Method == "GET" {
-		temp, err := template.ParseFiles("views/products/edit.html")
-		if err != nil {
-			panic(err)
-		}
+        ctx := r.Context()
 
-		IdString := r.URL.Query().Get("id")
-		id, err := strconv.Atoi(IdString)
-		if err != nil {
-			panic(err)
-		}
+        if r.Method == "GET" {
+                temp, err := template.ParseFiles("views/products/edit.html")
+                if err != nil {
+                        panic(err)
+                }
 
-		var product entities.Product
-		err = config.DB.QueryRow(`SELECT id, name, price, stock, description, category_id FROM products WHERE id = $1`, id).Scan(
-			&product.ID, &product.Name, &product.Price, &product.Stock, &product.Description, &product.CategoryID,
-		)
-		if err != nil {
-			http.Error(w, "Product not found", http.StatusNotFound)
-			return
-		}
+                IdString := r.URL.Query().Get("id")
+                id, err := strconv.Atoi(IdString)
+                if err != nil {
+                        panic(err)
+                }
 
-		categories := categorymodels.GetAll()
+                var product entities.Product
+                err = config.DB.QueryRow(`SELECT id, name, price, stock, description, category_id FROM products WHERE id = $1`, id).Scan(
+                        &product.ID, &product.Name, &product.Price, &product.Stock, &product.Description, &product.CategoryID,
+                )
+                if err != nil {
+                        http.Error(w, "Product not found", http.StatusNotFound)
+                        return
+                }
 
-		data := map[string]any{
-			"categories": categories,
-			"product":    product,
-		}
+                categories := categorymodels.GetAll(ctx)
 
-		temp.Execute(w, data)
+                data := map[string]any{
+                        "categories": categories,
+                        "product":    product,
+                }
 
-	}
+                temp.Execute(w, data)
 
-	if r.Method == "POST" {
-		err := r.ParseForm()
-		if err != nil {
-			panic(err)
-		}
+        }
 
-		idString := r.FormValue("id")
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			panic(err)
-		}
+        if r.Method == "POST" {
+                err := r.ParseForm()
+                if err != nil {
+                        panic(err)
+                }
 
-		price, _ := strconv.ParseFloat(r.FormValue("price"), 64)
-		stock, _ := strconv.Atoi(r.FormValue("stock"))
-		categoryID, _ := strconv.Atoi(r.FormValue("category_id"))
+                idString := r.FormValue("id")
+                id, err := strconv.Atoi(idString)
+                if err != nil {
+                        panic(err)
+                }
 
-		var product entities.Product
-		product.Name = r.FormValue("name")
-		product.Price = price
-		product.Stock = stock
-		product.CategoryID = uint(categoryID)
-		product.Description = r.FormValue("description")
-		product.UpdatedAt = time.Now()
+                price, _ := strconv.ParseFloat(r.FormValue("price"), 64)
+                stock, _ := strconv.Atoi(r.FormValue("stock"))
+                categoryID, _ := strconv.Atoi(r.FormValue("category_id"))
 
-		if ok := productmodels.Update(id, product); !ok {
-			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
-			return
-		}
+                var product entities.Product
+                product.Name = r.FormValue("name")
+                product.Price = price
+                product.Stock = stock
+                product.CategoryID = uint(categoryID)
+                product.Description = r.FormValue("description")
+                product.UpdatedAt = time.Now()
 
-		http.Redirect(w, r, "/product", http.StatusSeeOther) // ke halaman setelah update berhasil
-	}
+                if ok := productmodels.Update(ctx,id, product); !ok {
+                        http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+                        return
+                }
+
+                http.Redirect(w, r, "/product", http.StatusSeeOther) // ke halaman setelah update berhasil
+        }
 }
 
 func Delete(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	IdString := r.URL.Query().Get("id")
-	id, err := strconv.Atoi(IdString)
-	if err != nil {
-		panic(err)
-	}
+        ctx := r.Context()
 
-	if err := productmodels.Delete(id); err != nil {
-		panic(err)
-	}
+        IdString := r.URL.Query().Get("id")
+        id, err := strconv.Atoi(IdString)
+        if err != nil {
+                panic(err)
+        }
 
-	http.Redirect(w, r, "/product", http.StatusSeeOther)
+        if err := productmodels.Delete(ctx, id); err != nil {
+                panic(err)
+        }
+
+        http.Redirect(w, r, "/product", http.StatusSeeOther)
 }
