2add1cecd92697c75e8d8cf411bcdb228957be86
R4P8 <rizqirafa45@gmail.com>
2025-06-23 23:06:26 +0700

    feat(routes): JWT authentication endpoints

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"curd-web-go/controllers/authcontrollers"
 	"curd-web-go/controllers/categorycontrollers"
 	"curd-web-go/controllers/homecontrollers"
 	"curd-web-go/controllers/productcontrollers"
@@ -12,8 +13,16 @@ import (
 func Routes() *httprouter.Router {
 	router := httprouter.New()
 
+	//Home
 	router.Handler("GET", "/", middleware.Wrap(homecontrollers.Homecontrollers, "Home"))
 
+	//JWT
+	router.Handler("POST", "/auth/register", middleware.Wrap(authcontrollers.Register, "Register"))
+	router.Handler("POST", "/auth/login", middleware.Wrap(authcontrollers.Login, "Login"))
+	router.Handler("GET", "/auth/login", middleware.Wrap(authcontrollers.Login, "LoginGet"))
+	router.Handler("POST", "/auth/logout", middleware.Wrap(authcontrollers.Logout, "Logout"))
+
+	//Categories
 	router.Handler("GET", "/categories", middleware.Wrap(categorycontrollers.Index, "CategoriesIndex"))
 	router.Handler("GET", "/categories/add", middleware.Wrap(categorycontrollers.Add, "CategoriesAddGet"))
 	router.Handler("POST", "/categories/add", middleware.Wrap(categorycontrollers.Add, "CategoriesAddPost"))
@@ -21,6 +30,7 @@ func Routes() *httprouter.Router {
 	router.Handler("POST", "/categories/edit", middleware.Wrap(categorycontrollers.Edit, "CategoriesEditPost"))
 	router.Handler("GET", "/categories/delete", middleware.Wrap(categorycontrollers.Delete, "CategoriesDelete"))
 
+	//Product
 	router.Handler("GET", "/product", middleware.Wrap(productcontrollers.Index, "ProductIndex"))
 	router.Handler("GET", "/product/add", middleware.Wrap(productcontrollers.Add, "ProductAddGet"))
 	router.Handler("POST", "/product/add", middleware.Wrap(productcontrollers.Add, "ProductAddPost"))
@@ -30,5 +40,3 @@ func Routes() *httprouter.Router {
 
 	return router
 }
-
-
